bab83d87a10260d66f19e0adcddebbe3a5bc40aa
tawhan695 <36127485+tawhan695@users.noreply.github.com>
2022-01-09 19:38:50 +0700

    apo

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\KeyController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('login',[LoginController::class,'login']);
-Route::get('key',[App\http\Controllers\Api\KeyController::class, 'getday'])->middleware('auth');
+Route::get('key',[KeyController::class, 'getday'])->middleware('auth');
