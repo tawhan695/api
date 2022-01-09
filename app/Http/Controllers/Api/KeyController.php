67bf68eaad7e511e3aa210d0c6835d6dc55335f5
tawhan695 <36127485+tawhan695@users.noreply.github.com>
2022-01-09 19:25:02 +0700

    $key

--- app/Http/Controllers/Api/KeyController.php
+++ app/Http/Controllers/Api/KeyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Keytime;
+use App\Models\Key_has_user;
 class KeyController extends Controller
 {
     public function index(Request $request){
@@ -28,13 +29,16 @@ class KeyController extends Controller
         }
     }
     public function getday(Request $request){
-
-        $Keytime = Keytime::where('keytime',$request->token)->first();
-        if ($Keytime) {
+        $key = Key_has_user::join('keytimes','key_has_users.keytime_id', '=','keytimes.id')
+        ->where('user_id' ,auth()->user()->id)
+        ->select('keytimes.keytime','keytimes.days')
+        ->get();
+        // $Keytime = Keytime::where('keytime',$request->token)->first();
+        if ($key) {
 
             return response()->json([
-                'key' => $Keytime->keytime ,
-                'days' => $Keytime->days ,
+                'key' => $key->keytime ,
+                'days' => $key->days ,
                 'status_code' => 200,
                 ])
                 ->header('Content-Type', 'application/json','charset=utf-8');
